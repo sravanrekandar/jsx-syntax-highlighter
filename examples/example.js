db2df17ec99bc9835d7a21b2f9a235ca2ddb4b27
SRAVAN KUMAR REKANDAR <sravanrekandar@gmail.com>
2015-07-04 00:00:42 +0530

    used npm

---
+++ examples/example.js
@@ -0,0 +1,8 @@
+// load nsh
+var nsh      =  require('node-syntaxhighlighter')
+  , language =  require('jsx-syntaxhighlighter'); // usually load a bruns from nsh like : nsh.getLanguage('js')
+
+var code = require('fs').readFileSync('./sampleCode.jsx', 'utf-8');
+var result = nsh.highlight(code, language);
+
+console.log('Result:\n' + result);
--- examples/usage.js
+++
@@ -1,8 +0,0 @@
-// load nsh
-var nsh      =  require('node-syntaxhighlighter')
-  , language =  require('./lib/shBrushJsx'); // usually load a bruns from nsh like : nsh.getLanguage('js')
-
-var code = require('fs').readFileSync('./example.jsx', 'utf-8');
-var result = nsh.highlight(code, language);
-
-console.log('Result:\n' + result);
